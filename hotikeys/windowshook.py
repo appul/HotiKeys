ebe135e9e800d81f744241a4f49c81cad7b4432a
Apple <appul@users.noreply.github.com>
2016-09-14 19:12:04 +0200

    Remove polling, kek

--- hotikeys/windowshook.py
+++ hotikeys/windowshook.py
@@ -3,6 +3,8 @@ import ctypes
 import logging
 import sys
 import threading
+from _ctypes import byref
+from ctypes import wintypes
 from traceback import print_exception
 from typing import Any
 from typing import Callable
@@ -15,14 +17,18 @@ _win32_SetWindowsHookExA = ctypes.windll.user32.SetWindowsHookExA
 _win32_UnhookWindowsHookEx = ctypes.windll.user32.UnhookWindowsHookEx
 _win32_CallNextHookEx = ctypes.windll.user32.CallNextHookEx
 _win32_PeekMessageW = ctypes.windll.user32.PeekMessageW
+_win32_GetMessageW = ctypes.windll.user32.GetMessageW
+_win32_TranslateMessage = ctypes.windll.user32.TranslateMessage
+_win32_DispatchMessageW = ctypes.windll.user32.DispatchMessageW
 
 
 class WindowsHook(object):
-    def __init__(self, event, signature, handler):
+    def __init__(self, event, signature, handler, *, polling=False):
         self.event = event  # type: int
         self.signature = signature  # type: List[Any]
         self.handler = handler  # type: Callable[[Any], Any]
         self.hook_id = None  # type: int
+        self.polling = polling
 
     def hook(self):
         def on_event(*args, **kwargs):
@@ -38,11 +44,16 @@ class WindowsHook(object):
         pntr = func(on_event)
         hmod = _win32_GetModuleHandleA(None)
         self.hook_id = _win32_SetWindowsHookExA(self.event, pntr, hmod, 0)
-
         atexit.register(self.unhook)
 
+        msg = wintypes.MSG()
         while self.hook_id:
-            _win32_PeekMessageW(None, 0, 0, 0, 0)
+            if self.polling:
+                _win32_PeekMessageW(None, 0, 0, 0, 0)
+            else:
+                _win32_GetMessageW(byref(msg), 0, 0, 0)
+                _win32_TranslateMessage(byref(msg))
+                _win32_DispatchMessageW(byref(msg))
 
     def unhook(self):
         _win32_UnhookWindowsHookEx(self.hook_id)
